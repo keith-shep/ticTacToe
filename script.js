3b00c60bc2036c9614cc4610484245ad5d5572fd
Keith <kmson7@gmail.com>
2020-04-23 20:04:11 +0800

    added function threeinarow and isgridfull

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// const myArr = new Array(9);
-
-myArr = ['x',    , 'o',
-            , 'o',    ,
+myArr = [   ,    ,    ,
+            ,    ,    ,
             ,    ,    ,];
 
-const placeCounter = function (counter, position, arr) {
+const placeCounter = function(counter, position, arr) {
     if (
         (counter == "x" || counter == "o")
         && position > 0 
@@ -16,3 +14,71 @@ const placeCounter = function (counter, position, arr) {
         return "Click on an empty spot on the grid";
     }
 };
+
+
+const checkGameOver = function (arr) {
+
+    
+}
+
+
+arr1 = ["x", "o", "x",
+        "o", "x",    ,
+        "x",    , "o",];
+
+arr2 = ["o",    , "x",
+        "o", "x",    ,
+        "o",    , "x",];
+
+arr3 = ["x", "o", "x",
+        "o", "x", "o",
+        "o", "x", "o",];
+
+arr4 = ["x",    , "x",
+        "o", "o", "o",
+        "x",    , "o",];
+
+arr5 = ["x", "x", "x",
+        "o",    , "o",
+        "x", "o", "o",];
+
+row1 = ["x", "x", "x"]
+row2 = ["o", "o", "o"]
+row3 = ["x", "o", "x"]
+row4 = [   ,    , "x"]
+row5 = [   ,    ,    ]
+
+threeInARow = function(row) {
+
+        counterPiece = row[0];
+
+        if (   
+                row[0] != undefined &&
+                row[0] == row[1] && 
+                row[1] == row[2]
+        ) {
+                return counterPiece;
+        }
+}
+
+
+isGridFull = function(grid) {
+        occupiedSpaces = grid.filter((space) => space != undefined);
+        return occupiedSpaces.length == 9;
+}
+
+
+
+
+myListChecker = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+            ];
+
+
